cfeb34acc478e2f7f01589ffa8d2962f799b6056
sandorkristof <sandork@kkszki.hu>
2023-09-13 12:50:55 +0200

    Első cmmit

---
+++ ProductApp/Controllers/ProductController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using static ProductApp.Dtos;
+
+namespace ProductApp.Controllers
+{
+    [Route("product")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+
+        private static readonly List<ProductDto> products = new()
+        {
+            new ProductDto(Guid.NewGuid(),"Termék1",3500,DateTimeOffset.UtcNow),
+            new ProductDto(Guid.NewGuid(),"Termék2",2500,DateTimeOffset.UtcNow),
+            new ProductDto(Guid.NewGuid(),"Termék3",1600,DateTimeOffset.UtcNow)
+        };
+        [HttpGet]
+
+        public IEnumerable<ProductDto> GetAll()
+        {
+            return products;
+        }
+        [HttpGet("{id}")]
+        public ProductDto GetById(Guid id) 
+        {
+            var product = products.Where(x => x.Id == id).FirstOrDefault();
+
+            return product;
+        }
+        [HttpPost]
+
+        public ProductDto PostProduct(CreateProductDto createProduct)
+        {
+            var product = new ProductDto(
+                Guid.NewGuid(),
+                createProduct.ProductName,
+                createProduct.ProductPrice,
+                DateTimeOffset.UtcNow
+                );
+            products.Add(product);
+            return CreatedAtAction(nameof(GetAll),new ProductDto { Id=id},product);
+        }
+        [HttpPut("{id}")]
+        public ProductDto PullProduct(Guid id,UpdateProductDto updtaProduct)
+        {
+            var existingProduct=products.Where(x=>x.Id == id).FirstOrDefault();
+            var product = existingProduct with
+            {
+                ProductName = updtaProduct.ProductName,
+                ProductPrice = updtaProduct.ProductPrice,
+            };
+            var index=products.FindIndex(x=> x.Id == id);
+            products[index] = product;
+
+            return products[index];
+        }
+    }
+}
---
+++ ProductApp/Dtos.cs
@@ -0,0 +1,10 @@
+﻿namespace ProductApp
+{
+    public class Dtos
+    {
+        public record ProductDto(Guid Id,string ProductName,int ProductPrice,DateTimeOffset CreateTime);
+        public record CreateProductDto(string ProductName,int ProductPrice);
+        public record UpdateProductDto(string ProductName, int ProductPrice);
+
+    }
+}
